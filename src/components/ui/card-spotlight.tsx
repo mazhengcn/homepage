24d323968f17dd7773b2f05dfaeec77ad765c2fc
zhengma <mazhengcn@outlook.com>
2025-09-30 12:14:01 +0800

    fix: update CardSpotlight and Card components for improved styling and consistency

--- src/components/ui/card-spotlight.tsx
+++ src/components/ui/card-spotlight.tsx
@@ -53,10 +53,10 @@ export default function CardSpotlight({ title, content, date }: Props) {
 
   return (
     <div className="h-max">
-      <div className="spotlight-card group relative overflow-hidden rounded-xl border-none p-px transition-all duration-300 ease-in-out hover:shadow-lg">
-        <Card className="border-none bg-white/40 transition-all duration-300 ease-in-out group-hover:backdrop-blur-[20px] dark:bg-zinc-950/40">
+      <div className="spotlight-card group relative overflow-hidden rounded-xl p-px transition-all duration-300 ease-in-out hover:shadow-lg">
+        <Card className="border-none bg-orange-100/40 transition-all duration-300 ease-in-out group-hover:backdrop-blur-[20px] dark:bg-zinc-800/40">
           <CardHeader>
-            <CardTitle className="text-xl font-bold">{title}</CardTitle>
+            <CardTitle className="text-xl">{title}</CardTitle>
           </CardHeader>
           <CardContent>
             <p className="opacity-70">{content}</p>
@@ -73,7 +73,7 @@ export default function CardSpotlight({ title, content, date }: Props) {
             </div>
           </CardFooter>
         </Card>
-        <div className="blob absolute left-0 top-0 h-20 w-20 rounded-full bg-orange-300/60 opacity-0 blur-2xl transition-all duration-300 ease-in-out dark:bg-orange-400/60" />
+        <div className="blob absolute left-0 top-0 h-20 w-20 rounded-full bg-orange-500 opacity-0 blur-2xl transition-all duration-300 ease-in-out dark:bg-orange-500" />
         <div className="fake-blob absolute left-0 top-0 h-20 w-20 rounded-full" />
       </div>
     </div>
