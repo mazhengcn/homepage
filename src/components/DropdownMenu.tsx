a9283846b5d95bf8ce14c89c6c9c61a0f9fb38fc
Zheng Ma <zhengma@sjtu.edu.cn>
2025-10-07 22:04:22 +0800

    refactor: improve DropdownMenuWithIcon component structure and state management

--- src/components/DropdownMenu.tsx
+++ src/components/DropdownMenu.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Button } from '@/components/ui/button'
 import {
   DropdownMenu,
@@ -9,15 +8,26 @@ import {
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
 import { Menu as IconMenu } from 'lucide-react'
+import React, { useState } from 'react'
 
 interface Props {
   tags: string[]
 }
 
 export function DropdownMenuWithIcon({ tags }: Props) {
+  const [dropdownOpen, setDropdownOpen] = useState(false)
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild className="cursor-pointer">
+    <DropdownMenu
+      open={dropdownOpen}
+      onOpenChange={val => setDropdownOpen(val)}
+    >
+      <DropdownMenuTrigger
+        asChild
+        onClick={() => {
+          setDropdownOpen(val => !val)
+        }}
+        className="cursor-pointer"
+      >
         <Button variant="outline" size="icon">
           <IconMenu />
         </Button>
