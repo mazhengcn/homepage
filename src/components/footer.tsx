e575e8b46bf36bf90536e8c4b1a2e702c3500b0f
Zheng Ma <zhengma@sjtu.edu.cn>
2025-10-13 21:57:47 +0800

    feat: enhance Footer component with logo and update layout; improve tsconfig formatting

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,12 +1,21 @@
+import Image from "next/image"
 import Link from "next/link"
+import nextLogo from "../../public/next.svg"
 
 const today = new Date()
 
 export default function Footer() {
   return (
-    <footer className="mt-10 p-4 text-center text-zinc-500">
-      &copy; {today.getFullYear()} Built with{" "}
-      <Link href={"https://nextjs.org/"}>Next.js</Link>. All rights reserved.
+    <footer className="mt-10 p-4 text-center text-zinc-500 items-start flex gap-2 justify-center">
+      &copy; {today.getFullYear()} Powered by{" "}
+      <Link href={"https://nextjs.org/"}>
+        <Image
+          src={nextLogo}
+          alt="Next.js Logo"
+          className="inline w-16 dark:invert"
+        />
+      </Link>
+      . All rights reserved.
     </footer>
   )
 }
