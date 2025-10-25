30962632f7312fa7de28bbbdb900c511e85d57dd
Zheng Ma <mazhengcn@outlook.com>
2025-10-25 23:04:09 +0800

    Wrap research items in link and add chevron

--- app/(home)/research/page.tsx
+++ app/(home)/research/page.tsx
@@ -1,5 +1,4 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import { Button } from "@/components/ui/button"
 import {
   Item,
   ItemActions,
@@ -10,6 +9,7 @@ import {
   ItemTitle,
 } from "@/components/ui/item"
 import { source } from "@/lib/source"
+import { ChevronRightIcon } from "lucide-react"
 import Link from "next/link"
 
 export default function ResearchPage() {
@@ -33,44 +33,44 @@ export default function ResearchPage() {
         <ItemGroup className="not-prose flex flex-col gap-6">
           {pageTreeMeta.map((meta) => {
             return (
-              <Item key={meta.$id} variant="outline">
-                <ItemMedia className="group-has-[[data-slot=item-description]]/item:translate-y-0 group-has-[[data-slot=item-description]]/item:self-center">
-                  <div className="flex -space-x-2 *:data-[slot=avatar]:ring-2 *:data-[slot=avatar]:ring-background *:data-[slot=avatar]:grayscale">
-                    <Avatar className="hidden sm:flex">
-                      <AvatarImage
-                        src="https://github.com/shadcn.png"
-                        alt="@shadcn"
-                      />
-                      <AvatarFallback>CN</AvatarFallback>
-                    </Avatar>
-                    <Avatar className="hidden sm:flex">
-                      <AvatarImage
-                        src="https://github.com/maxleiter.png"
-                        alt="@maxleiter"
-                      />
-                      <AvatarFallback>LR</AvatarFallback>
-                    </Avatar>
-                    <Avatar>
-                      <AvatarImage
-                        src="https://github.com/evilrabbit.png"
-                        alt="@evilrabbit"
-                      />
-                      <AvatarFallback>ER</AvatarFallback>
-                    </Avatar>
-                  </div>
-                </ItemMedia>
-                <ItemContent>
-                  <ItemTitle className="text-lg">{meta.name}</ItemTitle>
-                  <ItemDescription className="text-lg">
-                    {/* @ts-expect-error this is intentional */}
-                    {meta.description}
-                  </ItemDescription>
-                </ItemContent>
-                <ItemActions>
-                  <Button size="sm" variant="outline" asChild>
-                    <Link href={`/docs/${meta.$id}`}>Learn more</Link>
-                  </Button>
-                </ItemActions>
+              <Item key={meta.$id} variant="outline" asChild>
+                <Link href={`/docs/${meta.$id}`}>
+                  <ItemMedia className="group-has-[[data-slot=item-description]]/item:translate-y-0 group-has-[[data-slot=item-description]]/item:self-center">
+                    <div className="flex -space-x-2 *:data-[slot=avatar]:ring-2 *:data-[slot=avatar]:ring-background *:data-[slot=avatar]:grayscale">
+                      <Avatar className="hidden sm:flex">
+                        <AvatarImage
+                          src="https://github.com/shadcn.png"
+                          alt="@shadcn"
+                        />
+                        <AvatarFallback>CN</AvatarFallback>
+                      </Avatar>
+                      <Avatar className="hidden sm:flex">
+                        <AvatarImage
+                          src="https://github.com/maxleiter.png"
+                          alt="@maxleiter"
+                        />
+                        <AvatarFallback>LR</AvatarFallback>
+                      </Avatar>
+                      <Avatar>
+                        <AvatarImage
+                          src="https://github.com/evilrabbit.png"
+                          alt="@evilrabbit"
+                        />
+                        <AvatarFallback>ER</AvatarFallback>
+                      </Avatar>
+                    </div>
+                  </ItemMedia>
+                  <ItemContent>
+                    <ItemTitle className="text-lg">{meta.name}</ItemTitle>
+                    <ItemDescription className="text-lg">
+                      {/* @ts-expect-error this is intentional */}
+                      {meta.description}
+                    </ItemDescription>
+                  </ItemContent>
+                  <ItemActions>
+                    <ChevronRightIcon className="size-4" />
+                  </ItemActions>
+                </Link>
               </Item>
             )
           })}
